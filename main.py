04c53453baca5bce31669a64249abf7bf2660971
Nick Dima <ncd2dq@gmail.com>
2018-04-16 20:01:07 -0400

    fixed ship looping for infinite playfield

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from constants import *
 from asteroid import Asteroid
 from ship import SpaceShip
 from vectors import Vector
-
+#SHIP_ACCEL
 def setup(WIDTH, HEIGHT):
     pygame.init()
     clock = pygame.time.Clock()
@@ -13,9 +13,8 @@ def setup(WIDTH, HEIGHT):
 
 def events(spaceship):
 
-    accelerate_vector = spaceship.ship_direction._direction()
-
     global game_exit
+    global angle_change
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -26,40 +25,44 @@ def events(spaceship):
 
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_UP:
-                pass
-            if event.key == pygame.K_DOWN:
-                pass
-            if event.key == pygame.K_RIGHT:
-                pass
-            if event.key == pygame.K_LEFT:
-                pass
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
+                accelerate_vector = spaceship.find_ship_orientation()
                 spaceship.boost(accelerate_vector)
             if event.key == pygame.K_DOWN:
                 pass
             if event.key == pygame.K_RIGHT:
-                pass
+                angle_change = 0
             if event.key == pygame.K_LEFT:
+                angle_change = 0
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                accelerate_vector = spaceship.find_ship_orientation()
+                spaceship.boost(accelerate_vector)
+            if event.key == pygame.K_DOWN:
                 pass
+            if event.key == pygame.K_RIGHT:
+                angle_change = 5
+            if event.key == pygame.K_LEFT:
+                angle_change = -5
 
 def main():
     display, clock = setup(GAME_WIDTH, GAME_HEIGHT)
     asteroids = [Asteroid() for iter in range(5)]
     spaceship = SpaceShip()
-    spaceship.loop_position()
+    global angle 
+    global angle_change
+    angle_change = 0
+    angle = 0
 
     while True:
         display.fill(BACKGROUND_COLOR)
         events(spaceship)
-        
         for asteroid in asteroids:
             asteroid.show(display)
             asteroid.move()
 
-        spaceship.show(display)
-        spaceship.update()
+        spaceship.run(display, angle, SHIP_LOOP_OFFSET)
 
+        angle += angle_change
         pygame.display.update()
         clock.tick(FPS)
         
--- ship.py
+++ ship.py
@@ -1,6 +1,8 @@
 from constants import *
 from vectors import Vector 
 import pygame
+import math
+import time
 
 '''
 vectors 
@@ -41,24 +43,23 @@ class SpaceShip(object):
         middle_y = GAME_HEIGHT / 2
         self.location = Vector((middle_x, middle_y))
         self.momentum_direction = Vector((0.0, 0.0))
-        self.ship_direction = Vector((0.0, -1.0))
+        self.ship_direction = Vector((0.0, -1.0)) # potentially useless
         self.acceleration = Vector((0.0, 0.0))
-        self.point_list = self.calculate_vertices()
+        self.point_list = []
+        self.calculate_vertices()
 
     def calculate_vertices(self):
         '''
         Calculates all 4 points that make up the spaceship
         '''
-        head = (self.location.x, self.location.y)
-        back = (self.location.x, self.location.y + SPACESHIP_HEIGHT - SPACESHIP_HEIGHT_OFFSET)
-        left = (self.location.x - SPACESHIP_RADIUS, self.location.y + SPACESHIP_HEIGHT)
-        right = (self.location.x + SPACESHIP_RADIUS, self.location.y + SPACESHIP_HEIGHT)
+        head = (self.location.x + SPACESHIP_RADIUS, self.location.y)
+        back = (self.location.x + SPACESHIP_RADIUS, self.location.y + SPACESHIP_HEIGHT - SPACESHIP_HEIGHT_OFFSET)
+        left = (self.location.x, self.location.y + SPACESHIP_HEIGHT)
+        right = (self.location.x + SPACESHIP_RADIUS * 2, self.location.y + SPACESHIP_HEIGHT)
         point_list = [head, left, back, right]
         self.point_list = point_list
 
-        return point_list
-
-    def loop_position(self):
+    def loop_position(self, loop_offset):
         ''' 
         This method calculates the center point of the ship by finding the midpoint between
         the head of the ship and the back indent of the ship
@@ -73,20 +74,94 @@ class SpaceShip(object):
         center_vector = vector_list[0] - vector_list[2]
         center_vector.x, center_vector.y = center_vector.x / 2, center_vector.y / 2
         midpoint = vector_list[0] - center_vector
-
+        #print(midpoint)
+        loop = False
+        if midpoint.x >= GAME_WIDTH + loop_offset: # Works
+            self.location = Vector((loop_offset * -1, self.location.y))
+            loop = True
+        elif midpoint.x <= loop_offset * -1:
+            self.location = Vector((GAME_WIDTH, self.location.y))
+            loop = True
+        elif midpoint.y <= loop_offset * -1:
+            self.location = Vector((self.location.x, GAME_HEIGHT))
+            loop = True
+        elif midpoint.y >= GAME_HEIGHT + loop_offset: # Works
+            self.location = Vector((self.location.x, loop_offset * -1))
+            loop = True
+        if loop:
+            print('loop')
+            self.calculate_vertices()
+            #time.sleep(3)
 
     def update(self):
         self.momentum_direction += self.acceleration
         self.location += self.momentum_direction
         self.acceleration = Vector((0.0, 0.0))
 
+    def find_ship_orientation(self):
+        #head left, back, right
+        #Returns the unit direction that the ship is oriented
+        ship_facing_vector = Vector(self.point_list[0]) - Vector(self.point_list[2])
+        ship_facing_vector._normalize()
+        return ship_facing_vector
 
     def boost(self, acceleration):
         self.acceleration = acceleration
 
-    def rotate(self, direction):
-        pass
+    def convert_to_relative(self, to_convert):
+        ''' This method converts all points in the point list
+        to relative to the center of the ship, so that you 
+        rotate around the center of the ship, not around the origin
+        '''
+
+        converted = []
+        center = (self.location.x + SPACESHIP_RADIUS, self.location.y + SPACESHIP_RADIUS)
+
+        for vertex in to_convert:
+            new_x = vertex[0] - center[0]  ## problem is here one is a float
+            new_y = vertex[1] - center[1]
+            converted.append((new_x, new_y))
+        return converted
+
+    def convert_to_map(self, to_convert):
+
+        converted = []
+        center = (self.location.x + SPACESHIP_RADIUS, self.location.y + SPACESHIP_RADIUS)
+
+        for vertex in to_convert:
+            new_x = vertex[0] + center[0]
+            new_y = vertex[1] + center[1]
+            converted.append((new_x, new_y))
+
+        return converted
+
+    def rotate(self, theta):
+        relative = self.convert_to_relative(self.point_list)
+        convFactor =  math.pi / 180
+        rotated = []
+
+        for vertex in relative:
+            new_x = vertex[0] * math.cos(theta * convFactor) - vertex[1] * math.sin(theta * convFactor)
+            new_y = vertex[0] * math.sin(theta * convFactor) + vertex[1] * math.cos(theta * convFactor)
+            rotated.append((new_x, new_y))
+
+        self.point_list = self.convert_to_map(rotated)
+        '''
+        new_location = (self.location.x, self.location.y)
+        relative = self.convert_to_relative(new_location)
+        new_x = relative[0] * math.cos(theta * convFactor) - relative[1] * math.sin(theta * convFactor)
+        new_y = relative[0] * math.sin(theta * convFactor) + relative[1] * math.cos(theta * convFactor)
+        converted_back = self.convert_to_map((new_x, new_y))
+        self.location = Vector((converted_back[0], converted_back[1]))
+        '''
 
     def show(self, display):
-        point_list = self.calculate_vertices()
-        pygame.draw.polygon(display, SPACESHIP_COLOR, point_list, 2)
+        pygame.draw.polygon(display, SPACESHIP_COLOR, self.point_list, 2)
+
+    def run(self, display, theta, ship_loop_offset):
+        # ignoring boost / update / loop_position
+        self.calculate_vertices()
+        self.rotate(theta)
+        self.update()
+        self.loop_position(ship_loop_offset)
+        self.show(display)
